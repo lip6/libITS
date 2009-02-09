7973600814c4eb74e6551a0f062d09ecff6f7955
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2009-02-09 21:50:08 +0000

    iterate through files of a directory and run all formula files against the corresponding net files

--- samples/bench.pl
+++ samples/bench.pl
@@ -3,25 +3,82 @@
 use Getopt::Std;
 use Cwd;
 use File::Basename;
+use File::Find;
 
+###############
+my $nusmv= "~/NuSMV/NuSMV-2.4.3-x86_64-linux-gnu/bin/NuSMV";
+my $default_checksog="../src/sog-its";
+#############
+
+getopt ('mdetba');
+
+#parsing the file list description
+my $dir = $opt_d or die "Specify target directory -d please! \n$usage" if (!defined $opt_a);
+
+# command line for tool pnddd
+my $cwd =  getcwd or die "Can't get cwd, used to decide where to do output.\n";
+
+
+my $DEFAULT_TIMEOUT = 120;
+# howto use this tool
+my $usage="Usage:\n bench.pl -d [directory base] \n"
+  . "\n other options : \n"
+  . " -e pathTochecksogexe (default : currentDir/$default_checksog \n"
+  . " -t timeout (default $DEFAULT_TIMEOUT seconds)";
+
+my $checksog_exe;
+if (defined $opt_e) {
+  if ($opt_e !~ /^\//) {
+    # not an absolute path
+    $checksog_exe = $cwd.'/'.$opt_e;
+  } else {
+    $checksog_exe = $opt_e;
+  }
+} else {
+  $checksog_exe = $cwd."/".$default_checksog ;
+}
+
+if (! -x $checksog_exe  && !defined $opt_a) {
+  die "File \'$checksog_exe\' does not seem to refer to the expected checksog executable. Use option -e.\n$usage";
+}
+
+my $timeout = defined $opt_t ? $opt_t :  $DEFAULT_TIMEOUT ;
 
 use strict 'vars';   # After obtaining $opt_xxx, which is not a local variable
 
 
-my $nusmv= "~/NuSMV/NuSMV-2.4.3-x86_64-linux-gnu/bin/NuSMV";
-my $checksog="../check-sog";
-my $formulaff="./philo/formula.ltl";
-my $model="./philo/philo20";
+sub workonfile {
+  my $ff = $_ ;
+  #  Work on all .net source files
+  if ( -f $ff && $ff =~/\.net$/  ) {
+    print "work on file $ff\n";
+    my @formulas ;
+    my $formff = dirname($ff)."/formula.ltl";
+      print "using formula file : $formff";
+    if ( -r $formff ) {
 
-open IN,$formulaff or die "Bad formula file name";
-while (my $line = <IN>) {
-  chomp $line;
-  # ../check-sog -Fformula -c -e invoice.cami.net 1
-  my $call = "$checksog -f\"$line\" -c  $model.net 1";
-
-  print STDERR $call."\n";
-  system($call);
+      push @formulas, $formff;
+    }
+    $formff = "$ff.ltl";
+    if ( -r $formff ) {
+      push @formulas, $formff;
+    }
+    foreach  my $formulaff (@formulas) {
+      open IN,$formulaff or die "Bad formula file name";
+      while (my $line = <IN>) {
+	chomp $line;
+	# ../check-sog -Fformula -c -e invoice.cami.net 1
+	my $call = "$checksog_exe -f\"$line\" -c  $ff";
+	
+	print STDERR $call."\n";
+	system($call);
+      }
+    }
+  }
 }
 
 
 
+#main block
+
+find(\&workonfile,$dir);
