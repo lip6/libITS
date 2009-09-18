888c1b77127e9df3cfbc47cd8fa4b2c32d69f3e8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2009-09-18 09:42:01 +0000

    files to handle fully symbolic LTL

---
+++ src/fsltl.hh
@@ -0,0 +1,40 @@
+#ifndef __FSLTL__HH__
+#define __FSLTL__HH__
+
+
+#include "ITSModel.hh"
+#include "tgba/tgba.hh"
+
+namespace its {
+
+
+  /** This class handles the construction of a synchronized product of an ITSModel with an ITS representation of a TGBA.
+   *  It builds a composite adapted to the problem and contains the SCC search algorithm. */
+  class fsltlModel : public ITSModel {
+
+  public :
+    // Play factory role for building ITS types from other formalisms
+    // Returns false and aborts if type name already exists.
+    // Create a type to hold a spot TGBA
+    bool declareType (const class spot::tgba * tgba);
+
+    // Build the composed system based on the TGBA and the main instance of the ITSModel.
+    // Precondition : we have built a model, invoked setInstance and setInstanceState, and declared a TGBA type.
+    // Side-effect : updates the main instance
+    // Returns false and abort if any preconditions are unfulfilled
+    bool buildComposedSystem ();
+
+    // Verify whether there are accepting paths in the product.
+    // Precondition : buildComposedSystem has been invoked.
+    // Returns the set of accepting states + suffix.
+    State findSCC ();
+
+
+  };
+
+
+}
+
+
+
+#endif
