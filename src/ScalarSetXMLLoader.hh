5fef76697ed97d61c6f5cec2bb78f50fc8d159b3
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2009-11-29 14:36:50 +0000

    a parser for a ScalarSet

---
+++ src/ScalarSetXMLLoader.hh
@@ -0,0 +1,19 @@
+#ifndef __SCALAR_XMLLOADER_HH__
+#define __SCALAR_XMLLOADER_HH__
+
+#include <string>
+
+#include <expat.h>
+
+#include "ITSModel.hh"
+
+class ScalarSetXMLLoader 
+{
+public:	
+  static void loadNodes(void * data, const XML_Char* Elt, const XML_Char** Attr);
+  static void loadArcs(void * data, const XML_Char* Elt, const XML_Char** Attr);
+  static its::ScalarSet * loadXML(const std::string & filename, its::ITSModel & model, bool isTimed=false);
+			
+};
+
+#endif
