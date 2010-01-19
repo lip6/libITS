7c1b2a36cc611aa0444c2bce296b316a76c2741a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2010-01-19 13:06:17 +0000

    heavy refactoring prior to introduction of fsltl into framework

--- src/sogtgbautils.hh
+++ src/sogtgbautils.hh
@@ -5,11 +5,15 @@
 #include <list>
 #include <string>
 
+#include "misc/timer.hh"
 #include "ltlast/formula.hh"
 #include "ltlast/atomic_prop.hh"
 #include "ltlvisit/apcollect.hh"
+#include "tgba/bdddict.hh"
 
 #include "ITSModel.hh"
+
+#include "sogtgba.hh"
 
 namespace sogits {
 
@@ -44,35 +48,105 @@ namespace sogits {
 
 
   enum sog_product_type {
+    FSLTL, // < The fully symbolic approach: encode tgba as an ITS.
     PLAIN_SOG, // < The basic algorithm with static parameters.
     SLOG, // < The (Symbolic) Local Obs Graph algorithm
     DSOG // < The Dynamic Symbolic Obs Graph algorithm
   } ;
 
-/// \brief Check if the Petri net \a n can produce at least one infinite
-/// sequence accepted by the formula \a f.
-///
-/// If this is the case and \a ce_expected is true, such a sequence is displayed.
-/// In other case, a message indicating if the test is satisfied or not is
-/// printed. The four last parameters specify options for the translation
-/// of the formula \a f in a TGBA as indicated for the function
-/// spot::ltl_to_tgba_fm.
-/// \dontinclude modelcheck.cpp
-/// \skipline void model_check
-/// \until } //
-  void model_check(its::ITSModel & m,
-		   const spot::ltl::formula* f,
-		   sog_product_type sogtype,
-		   const std::string& echeck_algo,
-		   bool ce_expected,
-		   bool fm_exprop_opt=false,
-		   bool fm_symb_merge_opt=true,
-		   bool post_branching=false,
-		   bool fair_loop_approx=false,
-		   const std::string & ltl_string ="formula",
-		   bool dotdump = false,
-		   bool scc_optim = true);
 
+
+  class LTLChecker {
+    
+    spot::ltl::atomic_prop_set *sap_;
+    const spot::ltl::formula* f_;
+    
+    // For bdd varnum to AP name in Spot
+    spot::bdd_dict dict_;
+
+    its::ITSModel * model_;
+    sogIts * sogModel_;
+
+    const spot::tgba* a_;
+    sog_tgba * systgba_;
+    const spot::tgba* tba_;
+    // options
+    std::string echeck_algo_;
+    bool ce_expected_;
+    bool fm_exprop_opt_;
+    bool fm_symb_merge_opt_;
+    bool post_branching_;
+    bool fair_loop_approx_;
+    std::string ltl_string_;
+    bool display_;
+    bool scc_optim_;
+
+    spot::timer_map timers;
+
+    bool buildTgbaFromformula ();
+    
+  public :
+    LTLChecker () : sap_(NULL), 
+		    f_(NULL), 
+		    dict_(), 
+		    model_(NULL), 
+		    sogModel_(NULL),
+		    a_(NULL),
+		    systgba_(NULL),
+		    tba_(NULL),
+		    fm_exprop_opt_(false),
+		    fm_symb_merge_opt_(true),
+		    post_branching_(false),
+		    fair_loop_approx_(false),
+		    ltl_string_("formula"), 
+		    display_(false), 
+		    scc_optim_(true) {}
+
+    ~LTLChecker();
+
+    void setFormula (const spot::ltl::formula* f) {
+      f_ = f;
+    }
+
+    void setModel (its::ITSModel * model) { 
+      model_ = model;
+    }
+
+    void setOptions (const std::string& echeck_algo,
+		     bool ce_expected,
+		     bool fm_exprop_opt=false,
+		     bool fm_symb_merge_opt=true,
+		     bool post_branching=false,
+		     bool fair_loop_approx=false,
+		     const std::string & ltl_string ="formula",
+		     bool dotdump = false,
+		     bool scc_optim = true) 
+    {
+      echeck_algo_ = echeck_algo;
+      ce_expected_ = ce_expected;
+      fm_exprop_opt_ = fm_exprop_opt;
+      fm_symb_merge_opt_ = fm_symb_merge_opt;
+      post_branching_ = post_branching;
+      fair_loop_approx_ = fair_loop_approx;
+      ltl_string_ = ltl_string;
+      display_ = dotdump;
+      scc_optim_ = scc_optim;    
+    }
+    
+    /// \brief Check if the Petri net \a n can produce at least one infinite
+    /// sequence accepted by the formula \a f.
+    ///
+    /// If this is the case and \a ce_expected is true, such a sequence is displayed.
+    /// In other case, a message indicating if the test is satisfied or not is
+    /// printed. The four last parameters specify options for the translation
+    /// of the formula \a f in a TGBA as indicated for the function
+    /// spot::ltl_to_tgba_fm.
+    /// \dontinclude modelcheck.cpp
+    /// \skipline void model_check
+    /// \until } //
+    void model_check(sog_product_type sogtype);
+
+  };
 
 } // namespace
 
