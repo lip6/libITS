f2c006f3ff89580b4e97c54e9f1cbe702bc179a1
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2010-06-14 16:20:16 +0000

    delegate getPredicate on implem

--- src/JSONPNetType.hh
+++ src/JSONPNetType.hh
@@ -59,6 +59,17 @@ public :
   void visit (class TypeVisitor * visitor) const {
     visitor->visitPNet(net_);
   }
+
+
+  /** The state predicate function : string p -> SHom.
+   *  returns a selector homomorphism that selects states verifying the predicate 'p'.
+   *  The syntax of the predicate is left to the concrete type realization.
+   *  The only constraint is that the character '.' is used as a namespace separator
+   *  and should not be used in the concrete predicate syntax.
+   *  Examples : P1.fork = 1 ; P2.P3.think > 0  etc... */
+  Transition getPredicate (Label predicate) const { return getConcrete()->getPredicate(predicate); }
+
+  
 };
 
 
