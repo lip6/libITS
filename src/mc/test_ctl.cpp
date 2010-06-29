282a9bef0182fef5b00b0a42f1c6fb0d00195f53
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2010-06-29 09:40:54 +0000

    updated libits source folders

--- src/mc/test_ctl.cpp
+++ src/mc/test_ctl.cpp
@@ -3,13 +3,13 @@
 #include "ITSModel.hh"
 #include "SDD.h"
 // romeo parser
-#include "XMLLoader.hh"
+#include "petri/XMLLoader.hh"
 // prod parser
-#include "Modular2ITS.hh"
+#include "petri/Modular2ITS.hh"
 // ITSModel parser
 #include "ITSModelXMLLoader.hh"
 // Cami parser
-#include "JSON2ITS.hh"
+#include "petri/JSON2ITS.hh"
 
 // SDD utilities to output stats and dot graphs
 #include "util/dotExporter.h"
