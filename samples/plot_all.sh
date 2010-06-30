5738f42bf07cd1d062d975c1db9d6edbbfc4cb7a
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2010-06-30 14:57:44 +0000

    more plots, use ec result to discriminate, use an argument to set work folder

--- samples/plot_all.sh
+++ samples/plot_all.sh
@@ -1,18 +1,22 @@
 # /bin/sh
 
-./graph.sh sog-dsog-time.eps SOG DSOG 7 test_bench/**/*.log
-./graph.sh sog-slap-time.eps SOG SLAP 7 test_bench/**/*.log
-./graph.sh sog-fsltl-time.eps SOG FSLTL 7 test_bench/**/*.log
-./graph.sh dsog-fsltl-time.eps DSOG FSLTL 7 test_bench/**/*.log
-./graph.sh slap-dsog-time.eps SLAP DSOG 7 test_bench/**/*.log
-./graph.sh slap-fsltl-time.eps SLAP FSLTL 7 test_bench/**/*.log
-./graph.sh slap-slapfst-time.eps SLAP SLAP-FST 7 test_bench/**/*.log
+./graph.sh sog-dsog-time.eps -v SOG DSOG 7 $1/**/*.log
+./graph.sh sog-slap-time.eps -v SOG SLAP 7 $1/**/*.log
+./graph.sh sog-fsltl-time.eps -v SOG FSLTL 7 $1/**/*.log
+./graph.sh dsog-fsltl-time.eps -v DSOG FSLTL 7 $1/**/*.log
+./graph.sh slap-dsog-time.eps -v SLAP DSOG 7 $1/**/*.log
+./graph.sh slap-fsltl-time.eps -v SLAP FSLTL 7 $1/**/*.log
+./graph.sh slap-slapfst-time.eps -v SLAP SLAP-FST 7 $1/**/*.log
+./graph.sh slap-slapfsa-time.eps -v SLAP SLAP-FSA 7 $1/**/*.log
+./graph.sh slapfst-slapfsa-time.eps -v SLAP-FSA SLAP-FST 7 $1/**/*.log
 
 
-./graph.sh sog-dsog-mem.eps SOG DSOG 8 test_bench/**/*.log
-./graph.sh sog-slap-mem.eps SOG SLAP 8 test_bench/**/*.log
-./graph.sh sog-fsltl-mem.eps SOG FSLTL 8 test_bench/**/*.log
-./graph.sh dsog-fsltl-mem.eps DSOG FSLTL 8 test_bench/**/*.log
-./graph.sh slap-dsog-mem.eps SLAP DSOG 8 test_bench/**/*.log
-./graph.sh slap-fsltl-mem.eps SLAP FSLTL 8 test_bench/**/*.log
-./graph.sh slap-slapfst-mem.eps SLAP SLAP-FST 8 test_bench/**/*.log
+./graph.sh sog-dsog-mem.eps -v SOG DSOG 8 $1/**/*.log
+./graph.sh sog-slap-mem.eps -v SOG SLAP 8 $1/**/*.log
+./graph.sh sog-fsltl-mem.eps -v SOG FSLTL 8 $1/**/*.log
+./graph.sh dsog-fsltl-mem.eps -v DSOG FSLTL 8 $1/**/*.log
+./graph.sh slap-dsog-mem.eps -v SLAP DSOG 8 $1/**/*.log
+./graph.sh slap-fsltl-mem.eps -v SLAP FSLTL 8 $1/**/*.log
+./graph.sh slap-slapfst-mem.eps -v SLAP SLAP-FST 8 $1/**/*.log
+./graph.sh slap-slapfsa-mem.eps -v SLAP SLAP-FSA 8 $1/**/*.log
+./graph.sh slapfst-slapfsa-mem.eps -v SLAP-FSA SLAP-FST 8 $1/**/*.log
