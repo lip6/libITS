9d2379cd71020bdd25ede427c77ed9a57576ec17
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-01-04 13:39:30 +0000

    a file with boost dependency isolated from ITSModel

---
+++ src/parser_json/json_model.cpp
@@ -0,0 +1,9 @@
+
+#include "ITSModel.hh"
+
+namespace its {
+  // Create a type to hold a Petri net, with hierarchical representation based on JSON description.
+  bool ITSModel::declareType (const class PNet & net, const json::Hierarchie * hier) {
+    return addType(new JsonPNetType(net,hier));
+  }
+}
