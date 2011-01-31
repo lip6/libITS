a85148c9121ba1e39f84179ac2d0c25b3dd3fde6
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-01-31 17:01:43 +0000

    cut libpopt out of dependencies

--- src/etf/runtime.h
+++ src/etf/runtime.h
@@ -16,7 +16,6 @@
 #include <setjmp.h>
 #include <errno.h>
 #include <stdint.h>
-#include <popt.h>
 
 typedef struct {
 	char* key;
@@ -27,11 +26,6 @@ typedef struct {
 \brief Find the value for a given key, or -1 if it does not exist.
  */
 extern int linear_search(si_map_entry map[],const char*key);
-
-/**
-\brief Parse a string that represents command line options.
- */
-extern void RTparseOptions(const char* argline,int *argc_p,char***argv_p);
 
 #define RTstackBottom HREstackBottom
 
@@ -48,37 +42,6 @@ extern void RTfree(void *rt_ptr);
 #define RT_NEW(sort) ((sort*)RTmallocZero(sizeof(sort)))
 
 extern void *RTdlsym (const char *libname, void *handle, const char *symbol);
-
-
-/**
-\brief Initialize the runtime library using popt.
-\param min_args The minimum number of arguments allowed.
-\param max_args The maximum number of arguments allowed, where -1 denotes infinite.
-*/
-extern void RTinitPopt(int *argc_p,char**argv_p[],const struct poptOption * options,
-	int min_args,int max_args,char*args[],
-	const char* pgm_prefix,const char* arg_help,const char* extra_help
-);
-
-/**
-\brief Return the next argument.
- */
-extern char* RTinitNextArg();
-
-
-/**
-\brief Print usage of tool.
-Due to the fact that we may have different extra string for help and usage,
-the function poptPrintUsage should not be called directly.
- */
-extern void RTexitUsage(int exit_code);
-
-/**
-\brief Print help of tool.
-Due to the fact that we may have different extra string for help and usage,
-the function poptPrintHelp should not be called directly.
- */
-extern void RTexitHelp(int exit_code);
 
 extern int RTverbosity;
 
