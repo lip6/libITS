62787bbb149bebb44d10f23c8a169f5a9db95446
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-02-04 18:52:55 +0000

    code becomes much simpler thanks to externalized options handling

--- bin/main.cpp
+++ bin/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 
+#include "Options.hh"
 // The ITS model referential
 #include "ITSModel.hh"
 #include "SDD.h"
@@ -80,30 +81,13 @@ void exhibitModel (ITSModel & model) {
 
 
 void usage() {
-  cerr << "Instantiable Transition Systems SDD/DDD Analyzer; package " << PACKAGE_STRING <<endl;
-  cerr << "This tool performs state-space generation of ITS, including extended timed Petri Nets allowing \n"
-       << "inhibitor,pre,post (hyper)arcs. " <<endl
-       << " The reachability set is computed using SDD/DDD, the Hierarchical Set Decision Diagram library, \n"
-       << " Please see README file enclosed \n"
-       << "in the distribution for more details. Input is a ROMEO xml model file, or one of the hard coded demo examples\n"
-       << "(see Samples dir for documentation and examples). \n \nOptions :" << endl;
-  cerr<<  "    -i path : specifies the path to input Romeo model " <<endl;
-  cerr<<  "    -p path : specifies the path to input Prod format model with possible module info pnddd style (xxx.net)" <<endl;
-  cerr<<  "    -xml path : use a XML encoded ITSModel file, as produced by Coloane.\n" ;
-  cerr<<  "    -c path : use a CAMI encoded ordinary P/T net, as produced by Coloane or Macao. Use -j in conjunction with this option.\n" ;
-#ifdef HAVE_BOOST_FLAG  
-  cerr<<  "    -j path : use a JSON encoded hierarchy description file for a CAMI model, as produced using PaToH.\n" ;
-#endif
+  usageInputOptions();
+  usageSDDOptions();
+  
+  cerr << "its-reach specific options for " << PACKAGE_STRING <<endl;
   cerr << "    --dump-order path : dump the currently used variable order to file designated by path and exit. \n" ;
-  cerr << "    --load-order path : load the variable order from the file designated by path. \n" ;
   cerr<<  "    -d path : specifies the path prefix to construct dot state-space graph" <<endl;
-  cerr<<  "    --sdd : privilege SDD storage." <<endl;
-  cerr<<  "    --ddd : privilege DDD (no hierarchy) encoding.[DEFAULT]" <<endl;
   cerr<<  "    -bmc XXX : use limited depth BFS exploration, up to XXX steps from initial state." << endl;
-  cerr<<  "    --no-garbage : disable garbage collection (may be faster, more memory)" <<endl;
-  cerr<<  "    -ssD2 INT : use 2 level depth for scalar sets. Integer provided defines level 2 block size." <<endl;
-  cerr<<  "    -ssDR INT : use recursive encoding for scalar sets. Integer provided defines number of blocks at highest levels." <<endl;
-  cerr<<  "    -ssDS INT : use alternative recursive encoding for scalar sets. Integer provided defines number of blocks at lowest level." <<endl;
   cerr<<  "    --quiet : limit output verbosity useful in conjunction with tex output --texline for batch performance runs" <<endl;
   cerr<<  "    --help,-h : display this (very helpful) helping help text"<<endl;
   cerr<<  "Problems ? Comments ? contact " << PACKAGE_BUGREPORT <<endl;
@@ -121,163 +105,58 @@ void bugreport () {
 
 int main (int argc, char **argv) {
 
- string pathromeoff;
- string pathprodff;
- string pathXMLff;
- string pathCAMIff;
- string pathjsonff;
- string pathorderinff = "order";
- bool doromeoparse = false;
- bool doprodparse = false;
- bool doXMLITSparse = false;
- bool doCAMIparse = false;
- bool dojsonparse = false;
- bool doloadorder = false;
-
- its::storage method = ddd_storage ;
- // parse command line args to get the options
 
  // instanciate a Model
  ITSModel model;
 
+ // echo options of run
  std::cout << "its-reach command run as :\n" << std::endl;
  for (int i=0;i < argc; i++) {
    std::cout << argv[i] << "  ";
  }
  std::cout << std::endl;
- 
- for (int i=1;i < argc; i++) {
-   if ( ! strcmp(argv[i],"-i") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for romeo xml file name please after " << argv[i-1]<<endl; usage() ;exit(1);}
-     pathromeoff = argv[i];
-     doromeoparse = true;
-   } else if ( ! strcmp(argv[i],"-p") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for Prod file name please after " << argv[i-1]<<endl; usage() ;exit(1);}
-     pathprodff = argv[i];
-     doprodparse = true;
-   } else if ( ! strcmp(argv[i],"-xml") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for ITSModel XML file name please after " << argv[i-1]<<endl; usage() ;exit(1);}
-     pathXMLff = argv[i];
-     doXMLITSparse = true;
-   } else if ( ! strcmp(argv[i],"-c") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for CAMI file name please after " << argv[i-1]<<endl; usage() ;exit(1);}
-     pathCAMIff = argv[i];
-     doCAMIparse = true;
-#ifdef HAVE_BOOST_FLAG  
-   } else if ( ! strcmp(argv[i],"-j") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for JSON file name please after " << argv[i-1]<<endl; usage() ;exit(1);}
-     pathjsonff = argv[i];
-     dojsonparse = true;
-#endif
-   } else if (! strcmp(argv[i],"-d") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for .dot file name please after " << argv[i-1]<<endl; usage() ; exit(1);}
-     pathdotff = argv[i];
-     dodotexport = true;
-   } else if (! strcmp(argv[i],"-bmc") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for BMC depth " << argv[i-1]<<endl; usage() ; exit(1);}
-     BMC = atoi(argv[i]); 
-   } else if (! strcmp(argv[i],"-ssD2") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for scalar strategy " << argv[i-1]<<endl; usage() ; exit(1);}
-     int grain = atoi(argv[i]);
-     model.setScalarStrategy(DEPTH1,grain);
-   }else if (! strcmp(argv[i],"-ssDR") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for scalar strategy " << argv[i-1]<<endl; usage() ; exit(1);}
-     int grain = atoi(argv[i]);
-     model.setScalarStrategy(DEPTHREC,grain);   
-   }else if (! strcmp(argv[i],"-ssDS") ) {
-     if (++i > argc) 
-       { cerr << "give argument value for scalar strategy " << argv[i-1]<<endl; usage() ; exit(1);}
-     int grain = atoi(argv[i]);
-     model.setScalarStrategy(SHALLOWREC,grain);
-   } else if (! strcmp(argv[i],"--help") || ! strcmp(argv[i],"-h")  ) {
-     usage(); exit(0);
-   } else if (! strcmp(argv[i],"--quiet")   ) {
-     beQuiet = true;
-   } else if (! strcmp(argv[i],"--ddd")   ) {
-     method = ddd_storage;
-     modelName = "d3:";
-     model.setStorage(ddd_storage);
-   } else if (! strcmp(argv[i],"--sdd")   ) {
-     method = sdd_storage ;
-     model.setStorage(sdd_storage);
-   } else if (! strcmp(argv[i],"--no-garbage")   ) {
-     with_garbage = false;  
-   } else if (! strcmp(argv[i],"--dump-order")   ) {
-     if (++i > argc) 
-       { cerr << "give path value for dump-order " << argv[i-1]<<endl; usage() ; exit(1);}
-     pathorderff = argv[i];     
-     dodumporder = true;
-   } else if (! strcmp(argv[i],"--load-order")   ) {
-     if (++i > argc) 
-       { cerr << "give path value for load-order " << argv[i-1]<<endl; usage() ; exit(1);}
-     pathorderinff = argv[i];     
-     doloadorder = true;
-   } else {
-     cerr << "Error : incorrect Argument : "<<argv[i] <<endl ; usage(); exit(0);
-   }
- }
- 
- 
- if (doprodparse) {
-   vLabel nname = RdPELoader::loadModularProd(model,pathprodff);
-   modelName += pathprodff ;
-   model.setInstance(nname, "main");
-   model.setInstanceState("init");
- } else if ( doromeoparse) {
-   // Parse the input file to build the system
-   
-   TPNet * pnet = XMLLoader::loadXML(pathromeoff, dojsonparse);
-   if (dojsonparse) {
-#ifdef HAVE_BOOST_FLAG  
-     json::Hierarchie * hier = new json::Hierarchie();
-     json::json_parse(pathjsonff, *hier);
-     model.declareType(*pnet,hier);
-          model.print(std::cerr);
-#endif
-   } else {
-     model.declareType(*pnet);
-     modelName += pathromeoff ;
-   }
-   model.setInstance(pnet->getName(),"main");
-   model.setInstanceState("init");	  
- } else if (doXMLITSparse) {
-   ITSModelXMLLoader::loadXML(pathXMLff, model);
- } else if (doCAMIparse) {
-    if (! dojsonparse) {
-      std::cerr << "You need to specify a hierarchy configuration file in JSON format." << std::endl;
-      exit(1);
-    } else {
-#ifdef HAVE_BOOST_FLAG  
-      JSONLoader::loadJsonCami (model, pathCAMIff, pathjsonff);
-#endif
-//         model.print(std::cerr);
-    } 
- } else {
-   std::cerr << "Please use -i, -p, -xml or -c option to specify input problem.\n" ;
-   usage();
-   exit(1);
- } 
 
- if (doloadorder) {
-   ifstream is (pathorderinff.c_str());
-   if (! model.loadOrder(is)) {
-     std::cerr << "Problem loading provided order file :" << pathorderinff << "\n";
-     exit(1);
+ // Build the options vector
+ std::vector<const char *> args;
+ for (int i=1;i < argc; i++) {
+   args.push_back(argv[i]);
+ }
+
+ // parse command line args to get the options 
+ handleInputOptions (args, model);
+ // we now should have the model defined.
+ 
+ bool with_garbage = true;
+ // Setup SDD specific settings
+ handleSDDOptions (args, with_garbage);
+
+ 
+ argc = args.size();
+ for (int i=0;i < argc; i++) {
+   if (! strcmp(args[i],"-d") ) {
+     if (++i > argc) 
+       { cerr << "give argument value for .dot file name please after " << args[i-1]<<endl; usage() ; exit(1);}
+     pathdotff = args[i];
+     dodotexport = true;
+   } else if (! strcmp(args[i],"-bmc") ) {
+     if (++i > argc) 
+       { cerr << "give argument value for BMC depth " << args[i-1]<<endl; usage() ; exit(1);}
+     BMC = atoi(args[i]); 
+   } else if (! strcmp(args[i],"--help") || ! strcmp(args[i],"-h")  ) {
+     usage(); exit(0);
+   } else if (! strcmp(args[i],"--quiet")   ) {
+     beQuiet = true;
+   } else if (! strcmp(args[i],"--dump-order")   ) {
+     if (++i > argc) 
+       { cerr << "give path value for dump-order " << args[i-1]<<endl; usage() ; exit(1);}
+     pathorderff = args[i];     
+     dodumporder = true;
    } else {
-     if (! beQuiet) {
-       std::cout << "Successfully loaded order from file " << pathorderinff << std::endl;
-     }
+     cerr << "Error : incorrect Argument : "<<args[i] <<endl ; usage(); exit(0);
    }
  }
+ 
+ 
 	
  exhibitModel(model);
 
