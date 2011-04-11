b2028f438c80712870567411a194fc85dac54e88
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2011-04-11 13:04:33 +0000

    ajuste sum_all.sh pour les options -R3f et -dR3

--- samples/sum_all.sh
+++ samples/sum_all.sh
@@ -1,15 +1,20 @@
+#!/bin/zsh
 
-echo SOG
-./sum.pl -f '^SOG' $1/**/*.log
-echo SOP
-./sum.pl -f '^SOP' $1/**/*.log
-echo SLAP
-./sum.pl -f '^SLAP,' $1/**/*.log
-echo SLAP-FSA
-./sum.pl -f '^SLAP-FSA,' $1/**/*.log
-echo SLAP-FST
-./sum.pl -f '^SLAP-FST' $1/**/*.log
-echo FSEL
-./sum.pl -f '^FSEL' $1/**/*.log
-echo FSOWCTY
-./sum.pl -f '^FSOWCTY' $1/**/*.log
+if test $# -ne 1; then
+  echo "syntax: sum_all.sh directory"
+  exit 1
+fi
+
+if test ! -d "$1"; then
+  echo "$1 is not a directory"
+  exit 1
+fi
+
+for i in SOG SOP \
+         SLAP SLAP-FSA SLAP-FST \
+         'FSEL -dR3' FSEL 'FSEL -R3f' \
+         'FSOWCTY -dR3' FSOWCTY 'FSOWCTY -R3f'
+do
+  echo "=== $i"
+  ./sum.pl -f "^$i," $1/**/*.log
+done
