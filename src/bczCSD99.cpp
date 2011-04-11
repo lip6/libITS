5da90e52a56cdea1aca39c3b6caaf77f137d2990
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2011-04-11 14:06:34 +0000

    add BCZ99

---
+++ src/bczCSD99.cpp
@@ -0,0 +1,153 @@
+#include <cassert>
+#include <algorithm>
+#include <ext/hash_map>
+#include <iostream>
+#include <sstream>
+#include "tgba/bddprint.hh"
+
+
+#include "apiterator.hh"
+
+#include "bczCSD99.hh"
+
+//#define TRACE
+
+#ifdef TRACE
+#define trace std::cerr
+#else
+#define trace while (0) std::cerr
+#endif
+
+
+namespace sogits {
+
+  // build the agregate inductively defined by
+  //  (ap&Trans + id)^*  & ap (m)
+  bcz_state::bcz_state(const sogIts & model, const GSDD& entryStates, bdd bddAP) 
+    : spot::state(),condition(bddAP) {
+    states = model.getSelector(bddAP) (entryStates);
+    succ = model.getNextRel() (states);
+    
+    trace << "------\nBuilt bczstate from " << entryStates.nbStates() << " initial states ;" << std::endl ;
+    trace << "With AP =" << bddAP << std::endl;
+    trace << "Obtained :" << *this ;
+    trace << "(succ == states) =" << (succ == states) << std::endl;
+    trace << "------\n";
+  } 
+  
+  GSDD bcz_state::get_succ() const {
+    return  succ;
+  }
+  
+
+  int bcz_state::compare(const state* other) const {
+    const bcz_state* m = dynamic_cast<const bcz_state*>(other);
+    if (!m)
+      return 1;
+    return (states < m->states) ? -1 : 
+      (states == m->states) ?  0 : 1 ;
+  } //
+
+  size_t bcz_state::hash() const {
+    return states.hash();
+  } //
+
+  spot::state* bcz_state::clone() const {
+    return new bcz_state(*this);
+  } //
+
+  const GSDD & bcz_state::get_states() const {
+    return states;
+  } //
+
+  std::ostream & bcz_state::print (std::ostream & os) const {
+    return (os << "(BczState : size =" << get_states().nbStates() << " succ.size = " << get_succ().nbStates() << ")" << std::endl);
+  }
+
+
+bcz_succ_iterator::bcz_succ_iterator(const sogIts& m, const bcz_state& s)
+  : model(m), 
+    from(s), 
+    it(APIteratorFactory::create()), 
+    succstates(from.get_succ()), 
+    current_succ(NULL) {
+  // set status of iterator to done() initially
+
+  // succstates : initialize Ext = states that serve as seed for successors
+  trace << "Built bcz_succ_iterator : " << *this << std::endl;
+}
+
+  bcz_succ_iterator::~bcz_succ_iterator () {
+    delete current_succ;
+  }
+
+
+  void bcz_succ_iterator::step() {
+    // iterate until a non empty succ is found (or end reached)
+    for (  ; ! it.done() ; it.next() ) {
+      bcz_state s (model, succstates, it.current() );
+      if ( s.get_states() != SDD::null ) {
+	current_succ = new bcz_state(s);
+	break;
+      }
+    }
+  }
+
+void bcz_succ_iterator::first() {
+
+  /// position "it" at first of ap bdd set
+  it.first();
+  step();
+}
+
+
+void bcz_succ_iterator::next() {
+  assert(!done());
+  it.next();
+  step();
+} //
+
+bool bcz_succ_iterator::done() const {
+  return  it.done() ;
+} //
+
+spot::state* bcz_succ_iterator::current_state() const {
+  assert(!done());
+  trace << "FIRING : " << it.current() << std::endl;
+  trace << "FROM " << from << std::endl;
+  return new bcz_state(*current_succ);
+} //
+
+bdd bcz_succ_iterator::current_condition() const {
+  assert(!done());
+  trace << "Succ iter" << *this << " asked for current cond= " << from.get_condition() << std::endl;
+  return from.get_condition();
+} // 
+
+
+bdd bcz_succ_iterator::current_acceptance_conditions() const {
+  assert(!done());
+  return bddfalse;
+} //
+
+  std::ostream & bcz_succ_iterator::print (std::ostream & os) const {
+    return (os << "BczSuccIter : from =" << from << std::endl);
+  }
+  
+
+
+} // namespace
+
+
+std::ostream & operator << (std::ostream & os, const sogits::bcz_state &s) {
+  return s.print(os);
+}
+
+
+std::ostream & operator << (std::ostream & os, const sogits::bcz_succ_iterator &s) {
+  return s.print(os);
+}
+
+
+
+
---
+++ src/bczCSD99.hh
@@ -0,0 +1,84 @@
+#ifndef BCZ_STATE_HH
+#define BCZ_STATE_HH
+
+
+#include "tgba/state.hh"
+#include "bdd.h"
+#include "SDD.h"
+#include "sogIts.hh"
+#include "tgba/bdddict.hh"
+#include "tgba/succiter.hh"
+#include "tgba/state.hh"
+
+#include "bdd.h"
+#include "apiterator.hh"
+
+namespace sogits {
+
+/** This hosts an implementation of Biere, Clarke, Zhu CSD'99 paper.
+    Multiple State and Single State Tableaux for Combining Local and Global Model Checking. In E.-R. Olderog, B. Steffen (Eds.), Correct System Design Recent Insights and Advances, special issue dedicated to Hans Langmaack, Lecture Notes in Computer Science (LNCS), vol. 1710, Springer 1999, ext. version of our SMC'99 paper.
+*/
+/// \brief Implementation of a \a spot::state.
+class bcz_state : public spot::state {
+public:
+  // build the agregate inductively defined by
+  //  (ap&Trans + id)^*  & ap (m)
+  bcz_state(const sogIts & model, const GSDD & m, bdd ap);
+  int compare(const state* other) const;
+  size_t hash() const;
+  state* clone() const;
+
+  // return states contained in this agregate : that verify AP
+  const GSDD & get_states() const;
+  // return any successor of a state in get_states 
+  GSDD get_succ() const;
+  // return the formula over AP used to create this agregate
+  bdd get_condition() const { return condition;}
+
+  // pretty print
+  std::ostream & print (std::ostream &) const ;
+
+private:
+  SDD states; ///< the frontier markings.
+  SDD succ;
+  bdd condition;
+};
+
+/// \brief Implementation of a \a spot::tgba_succ_iterator for a \a ::marking.
+class bcz_succ_iterator : public spot::tgba_succ_iterator {
+  public:
+  bcz_succ_iterator(const sogIts & m, const bcz_state& s);
+  virtual ~bcz_succ_iterator();
+
+    void first();
+    void step();
+    void next();
+    bool done() const;
+    spot::state* current_state() const;
+    bdd current_condition() const;
+    int current_transition() const;
+    bdd current_acceptance_conditions() const;
+  
+  // pretty print
+  std::ostream & print (std::ostream &) const ;
+private:
+
+  const sogIts& model; ///< The petri net.
+  const bcz_state & from; ///< The source state.
+
+  APIterator it;
+  its::State succstates;
+  bcz_state * current_succ;
+};
+
+
+
+}
+
+std::ostream & operator << (std::ostream & , const sogits::bcz_state &);
+
+
+std::ostream & operator << (std::ostream & , const sogits::bcz_succ_iterator &);
+
+
+#endif
