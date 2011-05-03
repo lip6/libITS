150e06a3b6fb78d164f99c8de19b837c575cdfed
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2011-05-03 12:52:12 +0000

    less methods

--- samples/plot_all.sh
+++ samples/plot_all.sh
@@ -17,18 +17,18 @@ meth[3]=BCZ99
 meth[4]=SLAP
 meth[5]=SLAP-FSA
 meth[6]=SLAP-FST
-meth[7]='FSEL -dR3'
-meth[8]='FSEL -R3f'
-meth[9]=FSEL
-meth[10]='FSOWCTY -dR3'
-meth[10]=FSOWCTY
-meth[11]='FSOWCTY -R3f'
+#meth[7]='FSEL -dR3'
+#meth[7]='FSEL -R3f'
+meth[7]=FSEL
+#meth[10]='FSOWCTY -dR3'
+#meth[11]=FSOWCTY
+meth[8]='FSOWCTY -R3f'
 
 
 #set -x
-for (( i = 1; i <= 11; i++ ))
+for (( i = 1; i <= 8; i++ ))
 do
-  for (( j=$(( $i+1 )); j <= 11; j++ ))
+  for (( j=$(( $i+1 )); j <= 8; j++ ))
     do
     ./graph.sh "${meth[$i]}-${meth[$j]}-time.png" -v "${meth[$i]}" "${meth[$j]}" 7 $1/**/*.log
     ./graph.sh "${meth[$i]}-${meth[$j]}-mem.png" -v "${meth[$i]}" "${meth[$j]}" 8 $1/**/*.log
