1afe9a27226d065b9c073e65dcae3e5b370ed595
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2011-05-06 14:19:31 +0000

    plus de plots 2 à 2

--- samples/allscores.sh
+++ samples/allscores.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
 
+
 if test $# -ne 1; then
   echo "syntax: allscores.sh logfile"
   exit 1
 fi
 
+# set -x
+
 f=tmp.$$
 grep -v '""' $1 > $f
 
+
+grep -v SLAP-FSA $f | grep 'FS[^AT]' | ./winner.pl 7 - > $1.fsalgos.time
+grep -v SLAP-FSA $f | grep 'FS[^AT]' | ./winner.pl 8 - > $1.fsalgos.mem
+
+grep -v SLAP-FSA $f | grep 'SO[GP]' | ./winner.pl 7 - > $1.sogsop.time
+grep -v SLAP-FSA $f | grep 'SO[GP]' | ./winner.pl 8 - > $1.sogsop.mem
+
+
 grep -vE '(SLAP-FSA|FSOWCTY|FSEL)' $f | grep -v 'X[^W]' | ./winner.pl 3 - > $1.products
+
 grep -v SLAP-FSA $f | grep -v 'X[^W]' | ./winner.pl 7 - > $1.nox.time
 grep -v SLAP-FSA $f | grep -v 'X[^W]' | ./winner.pl 8 - > $1.nox.mem
 (echo Header; grep -v SLAP-FSA $f | grep 'X[^W]') |
@@ -17,10 +29,11 @@ grep -v SLAP-FSA $f | grep -v 'X[^W]' | ./winner.pl 8 - > $1.nox.mem
 (echo Header; grep -v SLAP-FSA $f | grep 'X[^W]') |
                                         ./winner.pl 8 - > $1.x.mem
 
-for f in $1.products $1.nox.time $1.nox.mem $1.x.time $1.x.mem; do
+for f in $1.fsalgos.time $1.fsalgos.mem $1.sogsop.mem $1.sogsop.time $1.products $1.nox.time $1.nox.mem $1.x.time $1.x.mem; do
   prefix=`basename $f | sed 's/[_.]log//g'`
   ./plotscore.sh $prefix $f
 done
 
-rm -f $1.products $1.nox.time $1.nox.mem $1.x.time $1.x.mem
+rm -f *.time *.mem
+#rm -f $1.fsalgos.time $1.fsalgos.mem  $1.products $1.nox.time $1.nox.mem $1.x.time $1.x.mem
 
