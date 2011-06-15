8dd15e5559860f15f1de1528369ef0255ac4e8d0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-06-15 11:58:20 +0000

    TypeBasics now handles parse of limited Boolean formulae

--- src/TypeBasics.cpp
+++ src/TypeBasics.cpp
@@ -1,5 +1,6 @@
 #include "TypeBasics.hh"
 #include <algorithm>
+#include <cstring>
 
 using std::find;
 
@@ -47,6 +48,54 @@ bool TypeBasics::setDefaultState (Label def) {
   }
 }
 
+  Transition TypeBasics::getPredicate (Label predicate) const {
+    char  pred [predicate.size()+1];
+    strcpy(pred,predicate.c_str());
+
+    // Test for nesting
+    if (* pred == '(') {
+      // scan ahead for closing paren.
+      char * end = strstr(pred,")");
+      if (end == NULL) {
+	std::cerr << "Syntax Error : Mismatched paren problem in predicate : " << predicate << std::endl;
+	exit(1);
+      } else {
+	*end='\0';
+	Transition nested = getPredicate(pred+1);
+	if (end+1!='\0') {
+	  if (strncmp(end+1,"&&",2)) {
+	    return nested & getPredicate(end+3);
+	  } else if (strncmp(end+1,"||",2)) {
+	    return nested + getPredicate(end+3);
+	  } else {
+	    std::cerr << "Syntax Error : Expected operator && or || after predicate : (" << pred+1 << ") but encountered unexpected:" << end+1 << std::endl;
+	    exit(1);
+	  }
+	} else {
+	  return nested;
+	}
+      } 
+    } else {
+      // No nesting case
+      char * and_t = strstr(pred,"&&");
+      char * or_t = strstr(pred,"||");
+      if (! and_t && ! or_t) {
+	return getAPredicate(predicate);
+      } 
+      if ( and_t && (! or_t || and_t < or_t)) {
+	*and_t = '\0';
+	Transition left = getPredicate(pred);
+	return left & getPredicate(and_t+2);
+      } else {
+	// so we are sure that :
+	// ( or_t && (! and_t || or_t < and_t)) 
+	*or_t = '\0';
+	Transition left = getPredicate(pred);
+	return left + getPredicate(or_t+2);
+      }	
+    }
+    
+  }
 
 
 } // namespace
--- src/TypeBasics.hh
+++ src/TypeBasics.hh
@@ -39,6 +39,19 @@ namespace its {
     bool setDefaultState (Label def);
     
     // succs, getState, locals left pure virtual
+
+    /** The state predicate function : string p -> SHom.
+     *  returns a selector homomorphism that selects states verifying the predicate 'p'.
+     *  The syntax of the predicate is left to the concrete type realization.
+     *  The only constraint is that the character '.' is used as a namespace separator
+     *  and should not be used in the concrete predicate syntax.
+     *  Examples : P1.fork = 1 ; P2.P3.think > 0  etc... */
+    Transition getPredicate (Label predicate) const ;
+
+    /** Called by getPredicate above, which handles boolean conjunction and disjunction.*
+     *  Stands for getAtomicPredicate. */
+    virtual Transition getAPredicate (Label predicate) const = 0;
+
   } ;
 
 
