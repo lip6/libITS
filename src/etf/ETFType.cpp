5ab3eb7d915cba2d91226142898fabcbcdb93e3e
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-07-22 15:20:50 +0000

    now namedLocals returns something a bit more interesting

--- src/etf/ETFType.cpp
+++ src/etf/ETFType.cpp
@@ -48,6 +48,19 @@ labels_t EtfType::getVarSet () const {
 
     ETFTransition (const vector<int> & proj) :  proj(proj),value(SDD::null) {};
     
+    // returns the index of the variable in the projection
+    // e.g. if transition touches vars 0, 3 , 5 and variable 3 is the target pid "var"
+    // this will return 1 (i.e index of 3 in projection)
+    // return -1 if var is not touched by the transition
+    int concernsVariable (int var) const {
+      for (vector<int>::const_iterator it = proj.begin() ; it != proj.end() ; ++it ) {
+	if (*it == var) {
+	  return var;
+	}
+      }
+      return -1;
+    }
+
     void addEdge (int *src, int * dst) {
       GSDD element = SDD::one;
       int len = proj.size();
@@ -64,59 +77,65 @@ labels_t EtfType::getVarSet () const {
       return apply2k(value);
     }
 
-
+    
   };
 
+
+  void EtfType::compute_transitions (vector<ETFTransition> & transitions) const {
+    int N=lts_type_get_state_length(ltstype);  
+    
+    int nbevents = etf_trans_section_count(etfmodel);
+    // Initialize the relation
+    
+    for(int section=0;section<nbevents;section++){
+      
+      //    printf("group %4d/%d\n",section+1,nbevents);
+      
+      etf_rel_t trans=etf_trans_section(etfmodel,section);
+      
+      int src[N];
+      int dst[N];
+      int lbl[nbevents];
+      
+      vector<int> proj;
+      
+      ETFrelIterate(trans);
+      if (ETFrelNext(trans,src,dst,lbl)) {
+	for(int i=0 ; i < N; i++) {
+	  if (src[i]) {
+	    proj.push_back(i);
+	  }
+	}
+      }
+      int len = proj.size();
+      
+      transitions.push_back( ETFTransition(proj) );
+      
+      
+      ETFrelIterate(trans);
+      while(ETFrelNext(trans,src,dst,lbl)){
+	int psrc[len];
+	int pdst[len];
+	
+	for (int ii=0 ; ii < len ; ++ii) {
+	  psrc[ii] = src[proj[ii]]-1;
+	  pdst[ii] = dst[proj[ii]]-1;
+	}
+	
+	transitions[section].addEdge(psrc,pdst);
+      }
+    }
+    
+  }
+  
 /** state and transitions representation functions */
 /** Local transitions */
 Transition EtfType::getLocals () const {
 
-  int N=lts_type_get_state_length(ltstype);  
-
-  int nbevents = etf_trans_section_count(etfmodel);
-
 
   vector<ETFTransition> transitions;
-  // Initialize the relation
+  compute_transitions(transitions);
 
-  for(int section=0;section<nbevents;section++){
-
-//    printf("group %4d/%d\n",section+1,nbevents);
-
-    etf_rel_t trans=etf_trans_section(etfmodel,section);
-
-    int src[N];
-    int dst[N];
-    int lbl[nbevents];
-
-    vector<int> proj;
-    
-    ETFrelIterate(trans);
-    if (ETFrelNext(trans,src,dst,lbl)) {
-      for(int i=0 ; i < N; i++) {
-	if (src[i]) {
-	  proj.push_back(i);
-	}
-      }
-    }
-    int len = proj.size();
-
-    transitions.push_back( ETFTransition(proj) );
-
-
-    ETFrelIterate(trans);
-    while(ETFrelNext(trans,src,dst,lbl)){
-      int psrc[len];
-      int pdst[len];
-      
-      for (int ii=0 ; ii < len ; ++ii) {
-	psrc[ii] = src[proj[ii]]-1;
-	pdst[ii] = dst[proj[ii]]-1;
-      }
-      
-      transitions[section].addEdge(psrc,pdst);
-    }
-  }
   d3::set<GShom>::type toadd;
   for (vector<ETFTransition>::const_iterator it = transitions.begin() ; it != transitions.end() ; ++it ){
     toadd.insert(it->getShom());
@@ -163,7 +182,128 @@ State EtfType::getPotentialStates(State reachable) const {
    * Used in witness trace/counter example construction scenarios.
    **/
   void EtfType::getNamedLocals (namedTrs_t & ntrans) const {
-    ntrans.push_back(namedTr_t("ETF edges",getLocals()));
+    vector<int> proc_vars;
+    labels_t proc_names;
+
+    int N=lts_type_get_state_length(ltstype);  
+    int varindex = -1;
+    for (int i=0; i < N ; i++) {
+      vLabel var2 = lts_type_get_state_name(ltstype,i);
+      vLabel type = lts_type_get_state_type(ltstype,i);
+            
+      if (type != "byte") {
+	proc_vars.push_back(i);
+	proc_names.push_back(var2);
+//	std::cerr << "we have a process of type :" << var2 << ":" << type << std::endl ;
+      }
+    }
+    
+    vector<ETFTransition> transitions;
+    compute_transitions(transitions);
+
+    d3::set<GShom>::type toadd;
+
+    
+    // To hold a map from transition name "Env.obs->move" to transition homomorphism
+    typedef std::map<vLabel,Transition> namedTs_t;
+    typedef namedTs_t::const_iterator namedTs_it;
+    namedTs_t namedTs;
+
+
+    for (vector<ETFTransition>::const_iterator it = transitions.begin() ; it != transitions.end() ; ++it ){
+      // find "owner" process
+      int process = -1;
+      // index of owner process in transition projection
+      int pproj = -1;
+      for (vector<int>::const_iterator pid = proc_vars.begin() ; pid != proc_vars.end() ; ++pid) {
+	if ( (pproj = it->concernsVariable(*pid)) != -1) {
+	  process = *pid;
+	  break;
+	}
+      }
+      
+      // Every transition should belong to a process
+      assert( process != -1 );
+
+      // find state pair 
+      vLabel srcS;
+      vLabel targetS;
+
+      // source process state index
+      int src = -1;
+      // target process state index
+      int dest = -1;
+
+      // Grab the SDD that represents the 2k mapping for the process state variable
+      State procTr = extractPotential ( pproj ) ( it->value ) ;
+
+      std::cerr << "wroking on 2k SDD : " << procTr << std::endl;
+      
+      // downcast to DDD
+      const DDD * val = (const DDD *) (procTr.begin()->first) ;
+
+      // read values
+      src = val->begin()->first ;
+
+      dest = val->begin()->second.begin()->first ;
+      
+      // build a name for this transition, of the form : "P_Rbt1.observe->move"
+      vLabel tname = lts_type_get_state_name(ltstype,process) ;
+      tname += ".";
+      
+      std::cerr << "building a transition for process : " << tname << "  " << src << "/" << dest << std::endl;
+
+
+      // find the type id for this process
+      int typeId = -1;
+
+      int type_count=lts_type_get_type_count(ltstype);
+      vLabel procType = lts_type_get_state_type(ltstype,process);
+      for(int i=0;i<type_count;i++){
+	//      Warning(info,"dumping type %s",lts_type_get_type(ltstype,i));
+	vLabel type2 = lts_type_get_type(ltstype,i);
+	//      std::cerr << "testing type " << type2 <<" against "<< type << std::endl;
+	if (type2 == procType) {
+	  typeId = i;
+	  break ;
+	}
+      }
+
+      
+      {
+	chunk c=etf_get_value(etfmodel,typeId,src);
+	size_t len=c.len*2+3;
+	char str[len];
+	chunk2string(c,len,str);
+	
+	tname += str; 
+      }
+      tname += "->";
+      {
+	chunk c=etf_get_value(etfmodel,typeId,dest);
+	size_t len=c.len*2+3;
+	char str[len];
+	chunk2string(c,len,str);
+
+	tname += str ; 
+      }
+
+      // add to other transitions with same label
+      Transition effect = namedTs[tname] ;
+      if (effect == Transition::id) {
+	effect = Transition::null;
+      }
+      effect = effect + it->getShom();
+      namedTs[tname] = effect ;
+    }
+
+    // return  the pairs that consitute the map
+    for (namedTs_it it = namedTs.begin() ; it != namedTs.end() ; ++it ) {
+      ntrans.push_back(namedTr_t(it->first , it->second ));
+    }
+    
+    // Old version, very simple
+    // ntrans.push_back(namedTr_t("ETF edges",getLocals()));
   }
 
 Transition EtfType::getAPredicate (Label predicate) const {
