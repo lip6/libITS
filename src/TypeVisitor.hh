ed64f62bdc540396cbac786509fc79e757a4709b
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-11-25 18:23:36 +0000

    support for GAL type in visitor

--- src/TypeVisitor.hh
+++ src/TypeVisitor.hh
@@ -13,7 +13,21 @@ namespace its {
     virtual void visitComposite (const class Composite & net) = 0;
     virtual void visitScalar (const class ScalarSet & net) = 0;
     virtual void visitCircular (const class CircularSet & net) = 0;
+    virtual void visitGAL (const class GAL & net) = 0;
   };
+
+  // A default empty behavior for visitors not supporting all types.
+  class BasicTypeVisitor : public TypeVisitor {
+  public :
+    virtual ~BasicTypeVisitor() {};
+    // or also could be a TPNet
+    virtual void visitPNet (const class PNet & net) {};
+    virtual void visitComposite (const class Composite & net) {};
+    virtual void visitScalar (const class ScalarSet & net) {};
+    virtual void visitCircular (const class CircularSet & net) {};
+    virtual void visitGAL (const class GAL & net) {};
+  };
+
 
 }
 
