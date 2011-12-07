1cc8e56f9d1ef0b3bc8a8303d2220130141f42bf
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2011-12-07 18:04:03 +0000

    various additional tests (commented)

--- bin/test-gal.cpp
+++ bin/test-gal.cpp
@@ -43,9 +43,42 @@ int main (int argc, char **argv) {
 //   // grab initial state
    State init = philType->getState("init");
 
+   std::cerr << init << std::endl;
+
 //   // gen reachable
-   Transition fix = fixpoint ( locals + Transition::id);
-   State reach = fix(init);
+//    State reach ;
+//    State r2 = init;
+//    int i=0;
+//    do {
+//      reach = r2;
+//      r2 = r2 + locals (r2);
+//      std::cout << " iter "<<i++ << " nbstates " << r2.nbStates() << std::endl;
+//    } while (reach != r2 ) ;
+
+//    std::cout << reach;
+
+
+   
+//    Type::namedTrs_t nlocs ;
+//    philType->getNamedLocals(nlocs);
+
+//    Transition t14;
+//    for (Type::namedTrs_it it = nlocs.begin() ; it != nlocs.end() ; ++it) {
+//      if (it->first == "t14") {
+//        t14 = it->second;
+//        break;
+//      }
+//    }
+
+//    std::cout << "t14 : " << t14 << std::endl;
+//    State roverflow = t14 (reach);
+//    std::cout << " After t14 : " << std::endl << roverflow << std::endl;
+
+
+    Transition fix = fixpoint ( locals + Transition::id);
+    State reach = fix(init);
+   
+   
 
 //   // print stats
    Statistic stat (reach,"reach");
