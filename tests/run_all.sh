aa158dd93c5eee381bf0180f840a820276d042e0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-01-18 11:49:36 +0000

    added a shunt to skip tests if no exe built

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -2,10 +2,14 @@
 
 echo "##teamcity[testSuiteStarted name='its-reach.test']"
 
+if ! [ -f ../bin/its-reach ]
+then
+	echo "Build of ITS-REACH seems to have failed, skipping test suite"
+else
 for i in *.data ; do
     ./run_test.pl $i ;
 done;
-
+fi
 
 echo "##teamcity[testSuiteFinished name='its-reach.test']"
 
