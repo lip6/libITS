642092e9508885fc6615bc32165a6b745ae9243d
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-03-20 23:15:05 +0000

    remove declaration of compile helper function

--- src/divine/dveLoader.hh
+++ src/divine/dveLoader.hh
@@ -1,15 +1,12 @@
 #ifndef __DVE_LOADER_GAL__
 #define __DVE_LOADER_GAL__
 
-#include "divine/dvecompile.h"
 #include "gal/GAL.hh"
 
 
 namespace its {
 
   GAL * loadDve2Gal (Label inputff);
-  void compile (Label inputff);
-
 
 }
 
