f4134aee27b42f2bb4212cd6799882682bcb2e37
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-03-20 23:16:21 +0000

    now use the dve2GAL helper to bypass compiler

--- src/divine/dveLoader.cpp
+++ src/divine/dveLoader.cpp
@@ -1,4 +1,5 @@
 #include "dveLoader.hh"
+#include "divine/dve2GAL.hh"
 #include "divine/wibble/string.h"
 #include <dlfcn.h>
 
@@ -11,75 +12,86 @@ namespace its {
 typedef GAL* (*createGAL_t) (void);
 
 GAL * loadDve2Gal (Label inputff) {
-  compile(inputff);
-  vLabel objname = "./" + wibble::str::basename( inputff ) + ".so";
-
-  void *hndl = dlopen(objname.c_str(), RTLD_NOW);
-  if(hndl == NULL){
-    cerr << "Error during dlopen call: " << dlerror() << endl;
-    exit(-1);
-  }  
-  createGAL_t mkr = (createGAL_t) (dlsym(hndl, "createGAL") );
-  GAL * toret = (*mkr) ();
-  std::cout << "Model successfully loaded from binary object file " << objname << std::endl;
-  return toret;
-
-}
-
-
-
-  
-static void die( std::string bla ) 
-{
-  std::cerr << bla << std::endl;
-  exit( 1 );
-}
-
-
-static void run( std::string command ) {
-  int status = system( command.c_str() );
-  if ( status != -1 && WEXITSTATUS( status ) != 0 )
-    die( "Error running external command: " + command );
-
-}
-
-static void gplusplus( std::string in, std::string out, std::string flags = "" ) {
-  std::stringstream cmd;
-  std::string multiarch =
-#if defined(USE_GCC_M32)
-    "-m32 "
-#elif defined(USE_GCC_M64)
-    "-m64 "
-#else
-    ""
-#endif
-    ;
-  std::string os_dependant_flags =
-#if defined(__APPLE__)
-  "-bundle -flat_namespace -undefined suppress "
-#else
-  "-shared "
-#endif
-  ;
-  cmd << "g++ -O0 -fPIC " << os_dependant_flags << multiarch << flags << " -o " << out << " " << in;
-  std::cout << "Running compilation step :" << cmd.str() << std::endl;
-  run( cmd.str() );
-}
-  
-void compile (Label inputff) {
-  divine::dve_compiler compiler(true);
+  dve2GAL::dve2GAL loader;
   std::string modelName = wibble::str::basename(inputff); 
-  compiler.setModelName(modelName);
-  compiler.read( inputff.c_str() );
-  compiler.analyse();
-  
-  std::string outfile = wibble::str::basename( inputff ) + ".cpp";
-  std::ofstream out( outfile.c_str() );
-  compiler.setOutput( out );
-  compiler.print_generator();
+  loader.setModelName(modelName);
+  loader.read( inputff.c_str() );
+  loader.analyse();
 
-  std::cout << "Generated GAL model file :" << outfile << std::endl;
-  gplusplus( outfile, wibble::str::basename( inputff ) + ".so" , std::string("-g -I")+LIBDDD+" -I"+LIBITS+" ");
+  return loader.convertFromDve();
 }
+
+
+
+  /** Old g++ compiler reliant code was before :**/
+
+//   compile(inputff);
+//   vLabel objname = "./" + wibble::str::basename( inputff ) + ".so";
+
+//   void *hndl = dlopen(objname.c_str(), RTLD_NOW);
+//   if(hndl == NULL){
+//     cerr << "Error during dlopen call: " << dlerror() << endl;
+//     exit(-1);
+//   }  
+//   createGAL_t mkr = (createGAL_t) (dlsym(hndl, "createGAL") );
+//   GAL * toret = (*mkr) ();
+//   std::cout << "Model successfully loaded from binary object file " << objname << std::endl;
+//   return toret;
+  
+
+
+  
+// static void die( std::string bla ) 
+// {
+//   std::cerr << bla << std::endl;
+//   exit( 1 );
+// }
+
+
+// static void run( std::string command ) {
+//   int status = system( command.c_str() );
+//   if ( status != -1 && WEXITSTATUS( status ) != 0 )
+//     die( "Error running external command: " + command );
+
+// }
+
+// static void gplusplus( std::string in, std::string out, std::string flags = "" ) {
+//   std::stringstream cmd;
+//   std::string multiarch =
+// #if defined(USE_GCC_M32)
+//     "-m32 "
+// #elif defined(USE_GCC_M64)
+//     "-m64 "
+// #else
+//     ""
+// #endif
+//     ;
+//   std::string os_dependant_flags =
+// #if defined(__APPLE__)
+//   "-bundle -flat_namespace -undefined suppress "
+// #else
+//   "-shared "
+// #endif
+//   ;
+//   cmd << "g++ -O0 -fPIC " << os_dependant_flags << multiarch << flags << " -o " << out << " " << in;
+//   std::cout << "Running compilation step :" << cmd.str() << std::endl;
+//   run( cmd.str() );
+// }
+  
+// void compile (Label inputff) {
+//   divine::dve_compiler compiler(true);
+//   std::string modelName = wibble::str::basename(inputff); 
+//   compiler.setModelName(modelName);
+//   compiler.read( inputff.c_str() );
+//   compiler.analyse();
+  
+//   std::string outfile = wibble::str::basename( inputff ) + ".cpp";
+//   std::ofstream out( outfile.c_str() );
+//   compiler.setOutput( out );
+//   compiler.print_generator();
+
+//   std::cout << "Generated GAL model file :" << outfile << std::endl;
+//   gplusplus( outfile, wibble::str::basename( inputff ) + ".so" , std::string("-g -I")+LIBDDD+" -I"+LIBITS+" ");
+// }
 
 } // namespace its
