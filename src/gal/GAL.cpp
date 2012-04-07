e7e1c4a2276a9405742890aaf3047e2b84b08fdf
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-04-07 09:38:04 +0000

    arrays in GAL : pretty print and add

--- src/gal/GAL.cpp
+++ src/gal/GAL.cpp
@@ -68,12 +68,34 @@ namespace its {
     os << "    }" << endl;
   }
 
+  ArrayDeclaration::ArrayDeclaration (Label name, size_t size) : name_(name), size_(size) {
+      for (size_t i=0; i < size; ++i){
+	vars_.push_back( IntExpressionFactory::createArrayAccess( name, i).eval().getName() );
+      }
+  }
+
+
   std::ostream & operator<< ( std::ostream & os, const GAL & gal) {
     os << "GAL " << gal.getName() << endl;
     os << "{" << endl;
+
+    os << "  //arrays"<<endl;
+    for (GAL::arrays_it it = gal.arrays_begin() ; it != gal.arrays_end() ; ++it ) {
+      os  << "  int[" << it->size() << "] " << it->getName() << "= (" ;
+      for (ArrayDeclaration::vars_it jt = it->vars_begin() ; jt != it->vars_end() ; /* in loop */ ) {
+	os << gal.getVarValue(jt->getName()) ;
+	++jt;
+	if (jt != it->vars_end()) {
+	  os << ", ";
+	}
+      }
+      os << ") ;" << std::endl;
+    }
+    
+
     os << "  //variables"<<endl;
     for (GAL::vars_it it = gal.vars_begin() ; it != gal.vars_end() ; ++it ) {
-      os  << "  " << it->getName() << "=" << gal.getVarValue(it->getName()) << ";" << endl;
+      os  << "  int " << it->getName() << "=" << gal.getVarValue(it->getName()) << ";" << endl;
     }
 
     os << "  //transitions "<<endl;
@@ -86,6 +108,16 @@ namespace its {
     return os;
   }
 
-
+  void GAL::addArray (Label name, const vals_t & vals) 
+  {
+    
+    ArrayDeclaration ad = ArrayDeclaration(name,vals.size());
+    arrays_.push_back(ad);
+    int i = 0;
+    for (ArrayDeclaration::vars_it it = ad.vars_begin() ; it != ad.vars_end() ; ++it,++i ) {
+      init_.setVarValue(it->getName(), vals[i]);
+    }
+  } 
+  
 
 }
