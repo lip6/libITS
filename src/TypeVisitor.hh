cb65f98a534bbe386c861c4b049899dfe96e2404
Maximilien Colange <colange@lrde.epita.fr>
2012-06-25 22:53:07 +0000

    remove compiler warnings about unused parameters

--- src/TypeVisitor.hh
+++ src/TypeVisitor.hh
@@ -9,11 +9,11 @@ namespace its {
   public :
     virtual ~TypeVisitor() {};
     // or also could be a TPNet
-    virtual void visitPNet (const class PNet & net) = 0;
-    virtual void visitComposite (const class Composite & net) = 0;
-    virtual void visitScalar (const class ScalarSet & net) = 0;
-    virtual void visitCircular (const class CircularSet & net) = 0;
-    virtual void visitGAL (const class GAL & net) = 0;
+    virtual void visitPNet (const class PNet &) = 0;
+    virtual void visitComposite (const class Composite &) = 0;
+    virtual void visitScalar (const class ScalarSet &) = 0;
+    virtual void visitCircular (const class CircularSet &) = 0;
+    virtual void visitGAL (const class GAL &) = 0;
   };
 
   // A default empty behavior for visitors not supporting all types.
@@ -21,11 +21,11 @@ namespace its {
   public :
     virtual ~BasicTypeVisitor() {};
     // or also could be a TPNet
-    virtual void visitPNet (const class PNet & net) {};
-    virtual void visitComposite (const class Composite & net) {};
-    virtual void visitScalar (const class ScalarSet & net) {};
-    virtual void visitCircular (const class CircularSet & net) {};
-    virtual void visitGAL (const class GAL & net) {};
+    virtual void visitPNet (const class PNet &) {};
+    virtual void visitComposite (const class Composite &) {};
+    virtual void visitScalar (const class ScalarSet &) {};
+    virtual void visitCircular (const class CircularSet &) {};
+    virtual void visitGAL (const class GAL &) {};
   };
 
 
