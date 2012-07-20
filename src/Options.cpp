8b96cfa4bd8db1e7285721107487a79291f9df61
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2012-07-20 21:01:14 +0000

    simplified code due to new API for GAl parse

--- src/Options.cpp
+++ src/Options.cpp
@@ -31,9 +31,7 @@
 // Divine interaction
 #include "divine/dveLoader.hh"
 // GAL parser
-#include "gal/parser/exprParserLexer.h"
-#include "gal/parser/exprParserParser.h"
-#include <antlr3.h>
+#include "gal/parser/GALParser.hh"
 
 using std::string;
 using std::cerr;
@@ -246,47 +244,9 @@ bool handleInputOptions (std::vector<const char *> & argv, ITSModel & model) {
     }
   case GAL_T :
     {
-      // opens the file as an input stream for the lexer
-      pANTLR3_INPUT_STREAM input = antlr3FileStreamNew((pANTLR3_UINT8)(pathinputff.c_str()), 0);
-      if (input == NULL) {
-	cerr << "Unable to open the file of permutations : " << pathinputff << endl;
-	exit(1);
-      }
-      
-      // the lexer
-      pexprParserLexer lexer = exprParserLexerNew(input);
-      if (lexer == NULL) {
-	cerr << "Unable to create the lexer for the GAL file" << endl;
-	exit(1);
-      }
-    
-      // the token stream produces by the lexer
-      pANTLR3_COMMON_TOKEN_STREAM tstream = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lexer));
-      if (tstream == NULL) {
-	cerr << "Unable to allocate token stream for GAL parsing" << endl;
-	exit(1);
-      }
-    
-      // the parser
-      pexprParserParser parser = exprParserParserNew(tstream);
-      if (parser == NULL) {
-	cerr << "Unable to create the parser for the GAL" << endl;
-	exit(1);
-      }
     
       // do the parsing
-      GAL * result = parser->system(parser);
-      if (parser->pParser->rec->state->errorCount > 0) {
-	cerr << "The parser returned " << parser->pParser->rec->state->errorCount << " errors, parsing aborted" << endl;
-	exit(1);
-      }
-    
-      // free memory
-      parser->free(parser); parser = NULL;
-      tstream->free(tstream); tstream = NULL;
-      lexer->free(lexer); lexer = NULL;
-      input->close(input); input = NULL;
-
+      GAL * result = GALParser::loadGAL(pathinputff);
       model.declareType (*result);
       
       model.setInstance(result->getName(), "main");
