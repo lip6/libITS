d971e0cd07bbbe8fd67579018dc22dfc7fa875c7
Maximilien Colange <colange@lrde.epita.fr>
2013-03-25 14:38:21 +0000

    adapt the observer to the new interface

--- bin/main.cpp
+++ bin/main.cpp
@@ -45,17 +45,60 @@ static int BMC = -1;
 static size_t fixobs_passes = 5000;
 
 class EarlyBreakObserver : public fobs::FixObserver {
+  bool interrupted;
   size_t n;
   size_t max;
   Transition pred;
 public:
-  EarlyBreakObserver (size_t m, Transition p): n(0), max(m), pred(p) {}
+  EarlyBreakObserver (size_t m, Transition p)
+  : interrupted(false)
+  , n(0)
+  , max(m)
+  , pred(p)
+  {}
   
-  bool was_interrupted () const { return n > max; }
-  bool should_interrupt ()
+  bool was_interrupted () const { return interrupted; }
+  
+  bool
+  should_interrupt (const GSDD & after, const GSDD & before)
   {
-    return ++n > max;
+    if (interrupted)
+      return true;
+    if (n > max)
+    {
+      if (after != before)
+      {
+        interrupted = true;
+        return true;
+      }
+    }
+    else
+    {
+      ++n;
+    }
+    return false;
   }
+  
+  bool
+  should_interrupt (const GDDD & after, const GDDD & before)
+  {
+    if (interrupted)
+      return true;
+    if (n > max)
+    {
+      if (after != before)
+      {
+        interrupted = true;
+        return true;
+      }
+    }
+    else
+    {
+      ++n;
+    }
+    return false;
+  }
+
   void update (const GSDD & after, const GSDD & before)
   {
     if (pred (after) != State::null)
@@ -66,6 +109,7 @@ public:
     {
       n = 0;
       max += max;
+      interrupted = false;
       trace << "SDD proceeding with computation, new max is " << max << std::endl;
     }
   }
@@ -79,6 +123,7 @@ public:
     {
       n = 0;
       max += max;
+      interrupted = false;
       trace << "DDD proceeding with computation, new max is " << max << std::endl;
     }
   }
