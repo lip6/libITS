174f2d0207dd0e4cc9fa4def302e05f576efbdc6
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2015-03-05 10:29:52 +0000

    use Hom_Basic rather than ad hoc crafted solutions

--- src/petri/Hom_PlaceArcs.hh
+++ src/petri/Hom_PlaceArcs.hh
@@ -33,195 +33,6 @@
 #include "Hom.h"
 #include "Hom_Basic.hh"
 
-// **********************************************
-class _InhibitorPlace:public StrongHom {
- public:
-  int pl;
- private:
-  int pre;
-public:
-	_InhibitorPlace(int p, int pr) : pl(p), pre(pr) {}
-
-    bool
-    skip_variable(int var) const
-    {
-        return pl != var;
-    }
-
-	GDDD phiOne() const {
-		return GDDD::one;
-	}                   
-
-	GHom phi(int vr, int vl) const {
-		if (pl == vr)
-		{
-			if (vl < pre)
-				return GHom(vr, vl, GHom::id);
-			else
-				return GHom(GDDD::null);
-		}
-		else
-		{
-		    return GHom(vr, vl, GHom(this)); 
-	    }
-	}
-
-	size_t hash() const {
-		return 8097*pl^pre;
-	}
-
-	bool operator==(const StrongHom &s) const {
-		_InhibitorPlace* ps = (_InhibitorPlace*)&s;
-		return pl == ps->pl && pre == ps->pre;
-	}
-
-  _GHom * clone () const {  return new _InhibitorPlace(*this); }
-
-  void print (std::ostream & os) const {
-    os << "Inh(" << DDD::getvarName(pl) << "," << pre << ")";
-  }
-};
-
-// **********************************************
-class _PrePlace:public StrongHom {
-   public:
-  int pl;
- private:
-  int  pre;
-public:
-	_PrePlace(int p, int pr) : pl(p), pre(pr) {}
-
-	GDDD phiOne() const {
-		return GDDD::one;
-	}                   
-
-    bool
-    skip_variable(int var) const
-    {
-        return pl != var;
-    }
-    
-
-	GHom phi(int vr, int vl) const {
-		if (pl == vr)
-		{
-			if (vl >= pre)
-				return GHom(vr, vl - pre, GHom::id);
-			else
-				return GHom(GDDD::null);
-		}
-		else
-		{
-		    return GHom(vr, vl, GHom(this)); 
-	    }
-	}
-
-	size_t hash() const {
-		return 6619*pl^pre;
-	}
-
-	bool operator==(const StrongHom &s) const {
-		_PrePlace* ps = (_PrePlace*)&s;
-		return pl == ps->pl && pre == ps->pre;
-	}
-
-  _GHom * clone () const {  return new _PrePlace(*this); }
-
-  void print (std::ostream & os) const {
-    os << "Pre(" << DDD::getvarName(pl) << "," << pre << ")";
-  }
-
-};
-
-// **********************************************
-class _PostPlace:public StrongHom {
-   public:
-  int pl;
- private:
-  int  post;
-public:
-	_PostPlace(int p, int po) : pl(p), post(po) {}
-
-    bool
-    skip_variable(int var) const
-    {
-        return pl != var;
-    }
-    
-
-	GDDD phiOne() const {
-		return GDDD::one;
-	}                   
-
-	GHom phi(int vr, int vl) const {
-	  if (pl == vr) {
-	    // Just a trick to detect if we are going unbounded ...
-	    //	    if (vl > 1) std::cerr << "reached value " << vl << " for place " << DDD::getvarName(vr) << std::endl; 
-	      return GHom(vr, vl + post, GHom::id);
-	  }
-	  return GHom(vr, vl, GHom(this)); 
-	}
-
-	size_t hash() const {
-		return 9049*pl^post;
-	}
-
-	bool operator==(const StrongHom &s) const {
-		_PostPlace* ps = (_PostPlace*)&s;
-		return pl == ps->pl && post == ps->post;
-	}
-
-  _GHom * clone () const {  return new _PostPlace(*this); }
-
-  void print (std::ostream & os) const {
-    os << "Post(" << DDD::getvarName(pl) << "," << post << ")";
-  }
-
-};
-
-
-// **********************************************
-class _ResetPlace : public StrongHom {
-public:
-  int pl;
-	_ResetPlace(int p):pl(p) {}
-
-	GDDD phiOne() const {
-		return GDDD::one;
-	}                   
-
-    bool
-    skip_variable(int var) const
-    {
-        return pl != var;
-    }
-    
-
-	GHom phi(int vr, int vl) const {
-		if (pl == vr)
-			return GHom(vr, 0, GHom::id);
-		return GHom(vr, vl, GHom(this)); 
-	}
-
-	size_t hash() const {
-		return 6011*pl;
-	}
-
-	bool operator==(const StrongHom &s) const {
-		_ResetPlace* ps = (_ResetPlace*)&s;
-		return pl == ps->pl;
-	}
-
-  _GHom * clone () const {  return new _ResetPlace(*this); }
-
-  void print (std::ostream & os) const {
-    os << "Reset(" << DDD::getvarName(pl) << ")";
-  }
-
-};
-
-
-
 // Construct a Hom for a StrongHom _PostPlace
 inline GHom postPlace(int pl, int post){
   return incVar(pl, post);
@@ -230,23 +41,23 @@ inline GHom postPlace(int pl, int post){
 
 // Construct a Hom for a StrongHom _InhibitorPlace
 inline GHom inhibitorPlace(int pl, int pre){
-	return _InhibitorPlace(pl, pre);
+  return  varLtState(pl, pre);
 }
 
 // Construct a Hom for a StrongHom _PrePlace
 inline GHom prePlace(int pl, int pre){
-	return _PrePlace(pl, pre);
+  return incVar(pl, -pre) & varGeqState(pl,pre);
 }
 
 
 // Construct a Hom for a place with capacity
 inline GHom capacityPlace(int pl, int cap){
-	return _InhibitorPlace(pl, cap+1);
+     return varLeqState(pl,cap);
 }
 
 
 inline GHom resetPlace(int pl){
-   return _ResetPlace(pl);
+  return setVarConst(pl,0);
 }
 
 #endif // __HOM_ARCS__H_
