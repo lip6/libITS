a24d3f040b1289cc3cc390f9f477a156eaf5cdcd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2016-03-07 15:18:10 +0000

    bail out if exe has not been built

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -2,10 +2,14 @@
 
 echo "##teamcity[testSuiteStarted name='its-ctl.test']"
 
+if ! [ -f ../bin/its-ctl ]
+then
+	echo "Build of ITS-CTL seems to have failed, skipping test suite"
+else
 for i in *.data ; do
     ./run_test.pl $i ;
 done;
-
+fi
 
 echo "##teamcity[testSuiteFinished name='its-ctl.test']"
 
