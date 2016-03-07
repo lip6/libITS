9b4305f299327a8a1e7da71bec942b6be7a7d970
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2016-03-07 15:18:45 +0000

    bail out if its-ltl wasn't built

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -2,10 +2,13 @@
 
 echo "##teamcity[testSuiteStarted name='its-ltl.test']"
 
+if ! [ -f ../src/its-ctl ]
+then
+    echo "Build of ITS-LTL seems to have failed, skipping test suite"
+else
 for i in *.data ; do
     ./run_test.pl $i ;
 done;
-
 
 echo "##teamcity[testSuiteFinished name='its-ltl.test']"
 
