514079dc7cd01b43fec19491e3ec6cd150933b57
Alexandre Duret-Lutz <adl@lrde.epita.fr>
2016-03-13 18:43:38 +0000

    Also upgrade extractAP.cpp for spot-1.99.9

--- src/extractAP.cpp
+++ src/extractAP.cpp
@@ -1,41 +1,27 @@
 
 
 #include <iostream>
-#include <fstream>
-#include <list>
-#include <sstream>
-#include <string>
-#include <cstring>
-
-#include <bddx>
 #include <spot/tl/parse.hh>
-#include <spot/tl/apcollect.hh>
 
-using std::cerr;
-using std::string;
-using std::endl;
-
-
-int main (int argc, char ** argv) {
-  const char * ltl_string = argv[1];
-
-    // Initialize spot
-  spot::ltl::parse_error_list pel;
-
-  spot::ltl::formula* f = spot::ltl::parse(ltl_string, pel);
-  if (spot::ltl::format_parse_errors(std::cerr, ltl_string, pel)) {
-    f->destroy();
-    return 1;
-  }
-
-  spot::ltl::atomic_prop_set* sap = spot::ltl::atomic_prop_collect(f);
-
-  if (sap) {
-    spot::ltl::atomic_prop_set::iterator it;
-    for(it = sap->begin(); it != sap->end(); ++it) {
-      std::cout << (*it)->name() << std::endl;
+// Parse a formula and print its atomic propositions.
+int main (int argc, char ** argv)
+{
+  if (argc != 2)
+    {
+      std::cerr << "syntax: extractAP formula\n";
+      return 1;
     }
-  }
+
+  auto pf = spot::parse_infix_psl(argv[1]);
+  if (pf.format_errors(std::cerr))
+    return 1;
+
+  pf.f.traverse([](spot::formula f)
+		{
+		  if (f.is(spot::op::ap))
+		    std::cout << f.ap_name() << '\n';
+		  return false;
+		});
 
   return 0;
 }
