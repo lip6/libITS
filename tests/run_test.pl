cf3f46067eee6a7f3453e7d2032cb00e3843139f
Maximilien Colange <colange@lrde.epita.fr>
2016-03-18 17:04:39 +0000

    Improve test runner, allows to detect abnormal exit values.

--- tests/run_test.pl
+++ tests/run_test.pl
@@ -1,6 +1,5 @@
 #! /usr/bin/perl
 
-
 print "Running test : $ARGV[0] \n";
 open IN, "< $ARGV[0]";
 
@@ -25,10 +24,7 @@ while (my $line = <IN>) {
 
 close IN;
 
-# Now run the tool
-
-my $tmpfile = "$ARGV[0].tmp";
-
+# Now run the tool# 
 
 # print "syscalling : $call \n";
 print "##teamcity[testStarted name='$title']\n";
@@ -36,30 +32,50 @@ print "##teamcity[testStarted name='$title']\n";
 my @tested;
 
 my @outputs = ();
+my @results = `$call`;
 
-open IN, "($call) |" or die "An exception was raised when attempting to run "+$call+"\n";
-while (my $line = <IN>) {
+while (my $line = shift(@results)) {
 #  print "read : $line";
   push (@outputs,$line);
   if ($line =~ /Model ,\|S\| /) {
-    $line = <IN> or die "Unexpected end of file after stats readout";
+    $line = shift(@results) or die "Unexpected end of file after stats readout";
     chomp $line;
     @tested = split (/\,/,$line);
     last;
   }
 }
 
+# retrieve the exit value of the test
+my $failure;
+if ($? == 0) {
+  $failure = 0;
+} else {
+  $failure = 1;
+}
+
+my $returnvalue;
+
 if ( @nominal[1] != @tested[1] ) {
   print "@outputs\n";
   print "\n##teamcity[testFailed name='$title' message='regression detected' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
   print "Expected :  @nominal[1]  Obtained :  @tested[1] \n";
+  $returnvalue = 1;
+} elsif ( $failure ) {
+  print "@outputs\n";
+  print "\n##teamcity[testFailed name='$title' message='test did not exit properly' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
+  print "Expected :  @nominal[1]  Obtained :  @tested[1] \n"; 
+  my $exitval = $? >> 8;
+  print "test exited with value $exitval\n";
+  $returnvalue = 99;
 } else {
   print "##teamcity[buildStatisticValue key='testDuration' value='@tested[2]']\n";
   print "##teamcity[buildStatisticValue key='testMemory' value='@tested[3]']\n";
   print "Test successful : $title \n";
   print "Control Values/Obtained : \n$title\n@nominal\n@tested\n";
+  $returnvalue = 0;
 }
 
 
 print "##teamcity[testFinished name='$title']\n";
 
+exit $returnvalue;
