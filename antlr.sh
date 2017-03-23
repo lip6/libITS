c18f9dfc340441cd6797657015ded5f9b09069b8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-03-23 18:03:39 +0100

    from ddd web page

---
+++ antlr.sh
@@ -0,0 +1,28 @@
+
+# ---------------
+# install antlr
+# ---------------
+
+# create a dedicated install dir
+mkdir usr
+mkdir usr/local
+mkdir usr/local/lib
+mkdir usr/local/include
+
+
+# install antlr
+wget http://www.antlr3.org/download/antlr-3.4.tar.gz
+tar -xzvf antlr-3.4.tar.gz
+
+cp antlr-3.4/lib/antlr-3.4-complete.jar ./usr/local/lib/
+tmp=$(pwd)
+cd ./antlr-3.4/runtime/C
+./configure --prefix="$tmp"/usr/local --enable-64bit
+make
+make install
+cd -
+
+# ---------------
+# install libits related tools with :
+
+# ./configure   --with-antlrc="$tmp"/usr/local/   --with-antlrjar="$tmp"/usr/local/lib/antlr-3.4-complete.jar
