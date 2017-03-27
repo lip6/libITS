40cea7acc421b79a4c4eaea3e88a729aa5cc8d32
Maximilien Colange <colange@lrde.epita.fr>
2017-03-27 09:34:32 +0200

    Improve Gal2c to support call semantics.

--- its/gal/GAL2C.cpp
+++ its/gal/GAL2C.cpp
@@ -25,70 +25,113 @@ GAL2C::GAL2C(const GAL& g)
 }
 
 std::string
-GAL2C::new_label(const std::string& s) const
+GAL2C::new_label(const std::string& olds, const std::string& news) const
 {
-  auto it = vars2index.find(s);
+  auto it = vars2index.find(olds);
   if (it != vars2index.end())
-    return "s[" + std::to_string(it->second) + "]";
+    return news + "[" + std::to_string(it->second) + "]";
 
-  it = arrays2index.find(s);
+  it = arrays2index.find(olds);
   if (it != arrays2index.end())
-    return "(s + " + std::to_string(it->second) + ")";
+    return "(" + news + "+" + std::to_string(it->second) + ")";
 
-  throw std::runtime_error("variable " + s + " not found while compiling gal model");
+  throw std::runtime_error("variable " + olds + " not found while compiling gal model");
 }
 
 // to print the intexpression, it suffices to change the variable labels
 // use the createIntExpression method, only the environment changes
 env_t
-GAL2C::remap_env(const env_t& e) const
+GAL2C::remap_env(const env_t& e, const std::string& vname) const
 {
   // find the new environment
   env_t res(e.size());
   for (size_t i = 0; i != e.size(); ++i)
   {
     std::string v = IntExpressionFactory::getVar(e[i]);
-    res[i] = IntExpressionFactory::getVarIndex(new_label(v));
+    res[i] = IntExpressionFactory::getVarIndex(new_label(v, vname));
   }
   return res;
 }
 
 IntExpression
-GAL2C::remap_expr(const IntExpression& expr) const
+GAL2C::remap_expr(const IntExpression& expr, const std::string& vname) const
 {
-  return IntExpressionFactory::createIntExpression(expr.getExpr(), remap_env(expr.getEnv()));
+  return IntExpressionFactory::createIntExpression(expr.getExpr(), remap_env(expr.getEnv(), vname));
 }
 BoolExpression
-GAL2C::remap_expr(const BoolExpression& expr) const
+GAL2C::remap_expr(const BoolExpression& expr, const std::string& vname) const
 {
-  return BoolExpressionFactory::createBoolExpression(expr.getExpr(), remap_env(expr.getEnv()));
+  return BoolExpressionFactory::createBoolExpression(expr.getExpr(), remap_env(expr.getEnv(), vname));
 }
 
 class ActionPrinter : public StatementVisitor
 {
 public:
-  ActionPrinter(const GAL2C& g, std::ostream& o): g_(g), os(o) {}
+  ActionPrinter(const GAL2C& g, std::ostream& o): g_(g), os(o), next_(0) {}
   ~ActionPrinter() = default;
 
   void
   visitAssign(const Assignment& a) override
   {
-    os << "\t" << g_.remap_expr(a.getVariable()) << " = " << g_.remap_expr(a.getExpression()) << ";\n";
+    os << "\t\t" << g_.remap_expr(a.getVariable(), "s") << " = " << g_.remap_expr(a.getExpression(), "s") << ";\n";
   }
 
   void
   visitIncrAssign(const IncrAssignment& a) override
   {
-    os << "\t" << g_.remap_expr(a.getVariable()) << " += " << g_.remap_expr(a.getExpression()) << ";\n";
+    os << "\t\t" << g_.remap_expr(a.getVariable(), "s") << " += " << g_.remap_expr(a.getExpression(), "s") << ";\n";
   }
 
   void
   visitSequence(const Sequence& s) override
   {
-    for (auto it = s.begin(); it != s.end(); ++it)
+    // split the sequence into subsequences
+    // each subsequence contains at most one Call, and if so it is its last statement
+    // furthermore, subsequences should be maximal (at most one that contains no Call)
+    // TODO in full generality, Call above should be replaced by any non-deterministic Statement
+    std::vector<std::vector<const Statement*>> subsequences;
+    subsequences.push_back({});
+    for (const auto& stat : s)
     {
-      (*it)->acceptVisitor(*this);
+      subsequences.rbegin()->push_back(stat);
+      if (stat->getType() == its::CALL)
+        subsequences.push_back({});
     }
+    // if the last statement is a call, the last subsequence is empty: remove it.
+    if (subsequences.back().empty())
+      subsequences.pop_back();
+
+    // generate a callback for each subsequence
+    for (unsigned i = 0; i != subsequences.size(); ++i)
+    {
+      os << "\tauto call_" << i << " = [](int* s, TransitionCB callback, void* context)\n";
+      os << "\t{\n";
+      for (const auto& s : subsequences[i])
+        s->acceptVisitor(*this);
+      if (subsequences[i].back()->getType() != its::CALL)
+        os << "\t\tcallback(context, nullptr, s);\n";
+      os << "\t};\n";
+    }
+
+    os << "\tcount_context c = { 0, ctx };\n";
+    for (unsigned i = 0; i <= subsequences.size(); ++i)
+    {
+      os << "\tmycontext c" << i << " = { ";
+      if (i == 0)
+      {
+        os << "wrapper_count, cb, &c";
+      }
+      else
+      {
+        os << "call_" << subsequences.size() - i << ", wrapper, &c" << (i-1);
+      }
+      os << "};\n";
+    }
+
+    os << "\tint dst[" << g_.next_ << "];\n";
+    os << "\tstd::copy(in, in+" << g_.next_ << ", dst);\n";
+    os << "\twrapper(&c" << subsequences.size() << ", nullptr, dst);\n";
+    os << "\treturn c.nb;\n";
   }
 
   void
@@ -104,14 +147,19 @@ public:
   }
 
   void
-  visitCall(const Call&)
+  visitCall(const Call& c)
   {
-    throw std::runtime_error("call not handled");
+    for (auto tit = g_.gal_.trans_begin(); tit != g_.gal_.trans_end(); ++tit)
+    {
+      if (tit->getLabel() == c.getLabel())
+        os << "\t\tdo_" << tit->getName() << "(s, callback, context);\n";
+    }
   }
 
   void
   visitAbort()
   {
+    /* would not be hard to do nothing... */
     throw std::runtime_error("abort not handled");
   }
 
@@ -124,6 +172,8 @@ public:
 private:
   const GAL2C& g_;
   std::ostream& os;
+  int next_;
+  std::map<const its::Statement*, std::string> names_;
 };
 
 void
@@ -132,17 +182,38 @@ GAL2C::dump(std::ostream& os) const
   // headers
   os << "#include <algorithm>\n\n\n";
 
+  // spins interface
+  os << "class transition_info_t;\n";
+  os << "typedef void (*TransitionCB)(void* ctx, transition_info_t* transition_info, int* dst);\n";
+  os << "typedef void (*run_trans)(int*, TransitionCB, void*);\n";
+  os << "struct count_context { int nb; void* ctx; };\n";
+  os << "struct mycontext { run_trans f; TransitionCB cb; void* ctx; };\n\n";
+
+  os << "void wrapper(void* c, transition_info_t*, int* s)\n";
+  os << "{\n";
+  os << "\tmycontext* cc = (mycontext*)c;\n";
+  os << "\tcc->f(s, cc->cb, cc->ctx);\n";
+  os << "};\n\n";
+
+  os << "void wrapper_count(int* s, TransitionCB cb, void* c)\n";
+  os << "{\n";
+  os << "\tcount_context* cc = (count_context*)c;\n";
+  os << "\tcb(cc->ctx, nullptr, s);\n";
+  os << "\t++cc->nb;\n";
+  os << "}\n\n";
+
   // export transitions
   ActionPrinter ap(*this, os);
   for (auto tit = gal_.trans_begin(); tit != gal_.trans_end(); ++tit)
   {
     os << "bool enabled_" << tit->getName() << "(const int* s)\n";
     os << "{\n";
-    os << "\treturn " << remap_expr(tit->getGuard()) << ";\n";
+    os << "\treturn " << remap_expr(tit->getGuard(), "s") << ";\n";
     os << "}\n";
 
-    os << "void " << tit->getName() << "(int* s)\n";
+    os << "int do_" << tit->getName() << "(const int* in, TransitionCB cb, void* ctx)\n";
     os << "{\n";
+    os << "\tif (! enabled_" << tit->getName() << "(in)) return 0;\n\n";
     ap.visitSequence(tit->getAction());
     os << "}\n\n";
   }
@@ -152,7 +223,7 @@ GAL2C::dump(std::ostream& os) const
   os << "\tint* s = (int*)ss;\n";
   for (auto vit = gal_.state_begin(); vit != gal_.state_end(); ++vit)
   {
-    os << "\t" << new_label(vit->first) << " = " << vit->second << ";\n";
+    os << "\t" << new_label(vit->first, "s") << " = " << vit->second << ";\n";
   }
   os << "}\n\n";
 
@@ -201,24 +272,15 @@ GAL2C::dump(std::ostream& os) const
   os << "\treturn \"\";\n";
   os << "}\n\n";
 
-  // spins interface
-  os << "class transition_info_t;\n";
-  os << "typedef void (*TransitionCB)(void* ctx, transition_info_t* transition_info, int* dst);\n\n";
 
   // call all transitions
   os << "extern \"C\" int get_successors(void* , int* in, TransitionCB cb, void* ctx)\n";
   os << "{\n";
   os << "\tint res = 0;\n";
-  os << "\tint dst[" << next_ << "];\n";
   for (auto tit = gal_.trans_begin(); tit != gal_.trans_end(); ++tit)
   {
-    os << "\tif (enabled_" << tit->getName() << "(in))\n";
-    os << "\t{\n";
-    os << "\t\t++res;\n";
-    os << "\t\tstd::copy(in, in + " << next_ << ", dst);\n";
-    os << "\t\t" << tit->getName() << "(dst);\n";
-    os << "\t\tcb(ctx, nullptr, dst);\n";
-    os << "\t}\n";
+    if (tit->getLabel().empty())
+      os << "\tres += do_" << tit->getName() << "(in, cb, ctx);\n";
   }
   os << "\treturn res;\n";
   os << "}\n";
--- its/gal/GAL2C.hh
+++ its/gal/GAL2C.hh
@@ -15,20 +15,22 @@ public:
   dump(std::ostream& os) const;
 
   IntExpression
-  remap_expr(const IntExpression&) const;
+  remap_expr(const IntExpression&, const std::string&) const;
   BoolExpression
-  remap_expr(const BoolExpression&) const;
+  remap_expr(const BoolExpression&, const std::string&) const;
 private:
+  friend class ActionPrinter;
+
   const GAL& gal_;
   int next_;
   std::map<std::string, int> vars2index;
   std::map<std::string, int> arrays2index;
 
   std::string
-  new_label(const std::string&) const;
+  new_label(const std::string&, const std::string&) const;
 
   env_t
-  remap_env(const env_t&) const;
+  remap_env(const env_t&, const std::string&) const;
 
 };
 
