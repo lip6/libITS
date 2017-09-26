ee4e1638de35a04c36e33748ef9a455e568e3809
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-09-26 17:45:15 +0200

    error codes thru return value
    taking improvements to runner script from https://github.com/lip6/libITS/blob/master/tests/run_test.pl

--- tests/run_test.pl
+++ tests/run_test.pl
@@ -46,20 +46,40 @@ while (my $line = <IN>) {
   }
 }
 
+# retrieve the exit value of the test
+my $failure;
+if ($? == 0) {
+  $failure = 0;
+} else {
+  $failure = 1;
+}
+
+my $returnvalue;
+
 
 if ( @nominal[1] != @tested[1] ) {
   print "\n##teamcity[testFailed name='$title' message='regression detected' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
   print "Expected :  @nominal[1]  Obtained :  @tested[1] \n";
+  $returnvalue = 1;
+} elsif ( $failure ) {
+  print "@outputs\n";
+  print "\n##teamcity[testFailed name='$title' message='test did not exit properly' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
+  print "Expected :  @nominal[1]  Obtained :  @tested[1] \n"; 
+  my $exitval = $? >> 8;
+  print "test exited with value $exitval\n";
+  $returnvalue = 99;
 } else {
   print "##teamcity[buildStatisticValue key='testDuration' value='@tested[2]']\n";
   print "##teamcity[buildStatisticValue key='testMemory' value='@tested[3]']\n";
   print "Test successful : $title \n";
   print "Control Values/Obtained : \n$title\n@nominal\n@tested\n";
+  $returnvalue = 0;
 }
 
 foreach my $i  (0..$#refverdicts ) {
   if ( @refverdicts[$i] ne @verdicts[$i] ) {
      print "\n##teamcity[testFailed name='$title' message='regression detected' details='' expected='formula $i : @refverdicts[$i]' actual='@verdicts[$i]'] \n";
+     $returnvalue = 1;  
   } else {
     print "Test of formula $i successful.\n";
   }
@@ -68,3 +88,4 @@ foreach my $i  (0..$#refverdicts ) {
 
 print "##teamcity[testFinished name='$title']\n";
 
+exit $returnvalue;
