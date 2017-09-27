494147e02eb3043e7c5db304b262261f33ca9524
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-09-27 12:19:20 +0200

    update to integrate elements from runner script of its-ctl

--- tests/run_test.pl
+++ tests/run_test.pl
@@ -51,15 +51,36 @@ while (my $line = <IN>) {
   }
 }
 
+# retrieve the exit value of the test
+my $failure;
+if ($? == 0) {
+  $failure = 0;
+} else {
+  $failure = 1;
+}
+
+my $returnvalue = 0;
 
 foreach my $i  (0..$#refverdicts ) {
   if ( @refverdicts[$i] ne @verdicts[$i] ) {
     print "##teamcity[testFailed name='$title' message='regression detected' details='formula $i' expected='@refverdicts[$i]' actual='@verdicts[$i]'] \n";
+    $returnvalue = 1;
+  } elsif ( $failure ) {
+    print "@outputs\n";
+    print "\n##teamcity[testFailed name='$title' message='test did not exit properly' details='' expected='@nominal[1]' actual='@tested[1]'] \n";
+    print "Expected :  @nominal[1]  Obtained :  @tested[1] \n"; 
+    my $exitval = $? >> 8;
+    print "test exited with value $exitval\n";
+    $returnvalue = 99;
   } else {
     print "Test of formula $i successful.\n";
+    print "Control Values/Obtained : \n$title\n@refverdicts\n@verdicts\n";
   }
 }
 
 
+
 print "##teamcity[testFinished name='$title']\n";
+
+exit $returnvalue;
 
